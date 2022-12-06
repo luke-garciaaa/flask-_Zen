a89d37a2d74d10e3ddd5a58e6932f34235fdc5ca
luke-garciaaa <luke.garcia2@laverne.edu>
2022-12-06 14:33:18 -0800

    try to return home page

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return jsonify({"Choo this should be the branch": "Welcome to your Flask app 🚅"})
+def home_page():
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
